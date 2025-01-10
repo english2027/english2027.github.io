86aa248f2c8276986421aa9a162105d40f97454a
english2027 <gulonatalia2016@gmail.com>
2025-01-10 19:20:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+﻿        let sendButton = window.document.getElementById('sendButton');
+        let inp = window.document.getElementById('textInput');
+        let outp = window.document.getElementById('textOutput');
+        let conversation = [];
+
+        let speechRecognizer = new webkitSpeechRecognition();        
+        let speechSynthesis = window.speechSynthesis;                
+
+        const speech = () => {
+            speechRecognizer.lang = 'en-EN';
+            speechRecognizer.start();
+            sendButton.innerText = 'Speak...';
+        }
+
+        const talk = (text) => {
+            let textToTalk = new SpeechSynthesisUtterance(text);
+            textToTalk.onend = function(event) {
+                sendButton.innerText = 'Do you have anything else you want to say? Click here - and speak up';
+            };
+            textToTalk.lang = 'en-EN';
+            textToTalk.rate = 0.5;
+            speechSynthesis.speak(textToTalk);
+        }
+
+        speechRecognizer.onresult = (event) => {                    
+            inp.value = event.results[0][0].transcript;
+            requestFunc();
+        }
+
+        const requestFunc = () => {
+            if (inp.value) {
+                sendButton.innerText = 'Wait...';
+                let message = {
+                    "role": "user",
+                    "content": inp.value
+                }
+                conversation.push(message);
+
+                // Отправка на сервер
+                axios.post('http://openai-server-n8xl.onrender.com/api/chat', { messages: conversation })
+                    .then(response => {
+                        let aiResponse = response.data.choices[0].message.content;
+                        outp.value = aiResponse;
+
+                        let gptMessage = {
+                            "role": "assistant",
+                            "content": aiResponse
+                        }
+                        conversation.push(gptMessage);
+                        talk(aiResponse);
+                    })
+                    .catch(error => {
+                        console.error("Error request:", error.message);
+                        sendButton.innerText = 'Error. Try again.';
+
+                    });
+            }
+        }
